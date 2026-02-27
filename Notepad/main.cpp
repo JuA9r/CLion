9d53a4334075a5546e935f8fdd34e4469ed7e431
JuA9r <kagasumiy01@gmail.com>
2026-02-27 21:22:04 +0900

    Initial commit

---
+++ Notepad/main.cpp
@@ -0,0 +1,96 @@
+#define STRICT
+
+#include <windows.h>
+#include <stdlib.h>
+#include <string.h>
+#include <tchar.h>
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+HINSTANCE hInst;
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI _tWinMain(HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine,
+    int nCmdShow) {
+
+    static TCHAR szWindowClass[] = _T("Sample");
+    static TCHAR szTitle[] = _T("Sample window");
+
+    WNDCLASSEX wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+    if (!RegisterClassEx(&wcex))
+    {
+        MessageBox(NULL,
+            _T("Failed to register window class."),
+            _T("Sample"),
+            NULL
+            );
+
+        return -1;
+    }
+    hInst = hInstance;
+
+    HWND hWnd = CreateWindow(
+        szWindowClass,
+        szTitle,
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        WINDOW_WIDTH, WINDOW_HEIGHT,
+        NULL,
+        NULL,
+        hInstance,
+        NULL
+        );
+
+    if (!hWnd)
+    {
+        MessageBox(NULL,
+            _T("Failed to create window."),
+            _T("Sample"),
+            NULL);
+        return -1;
+    }
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd,UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+    default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+            break;
+    }
+
+    return 0;
+}
